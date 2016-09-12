586cb7f473636f9f7b3db52f4d88fbdcbd075211
Jon Normington <jnormington@users.noreply.github.com>
2016-09-12 22:50:59 +0100

    Simplify the api actions find method for nested models

--- lib/clubhouse/api_actions.rb
+++ lib/clubhouse/api_actions.rb
@@ -2,35 +2,9 @@ module Clubhouse
   class NoSuchResourceError < StandardError; end
 
   module APIActions
-    def find(resource, id)
-      res = find_resource(resource)
-      payload = get("#{res.endpoint}/#{id}")
-      res.new.update_object_from_payload(payload)
-    end
-
-    def known_resources
-      return @resources if @resources
-
-      @resources = {}
-
-      Clubhouse.constants.each do |c|
-        klazz = Clubhouse.const_get(c.to_s)
-        if klazz.respond_to?(:endpoint)
-          @resources[klazz.name.split('::').last.underscore.to_sym] = klazz
-          @resources[klazz.endpoint] = klazz
-        end
-      end
-
-      @resources
-    end
-
-    private
-
-    def find_resource(resource)
-      res = known_resources[resource.to_sym]
-
-      return res if res
-      raise NoSuchResourceError, "Available resources are #{known_resources.keys}"
+    def find(clazz, path, id)
+      payload = get("#{path}/#{id}")
+      clazz.new.update_object_from_payload(payload)
     end
   end
 end
--- spec/unit/api_actions_spec.rb
+++ spec/unit/api_actions_spec.rb
@@ -13,27 +13,14 @@ module Clubhouse
     end
 
     describe '#find' do
-      let(:story) { subject.find(:story, 1) }
+      let(:story) { subject.find(Story, 'projects/1/stories', 1) }
 
       it 'calls get and returns resource object' do
-        expect(subject).to receive(:get).with('stories/1').and_return({"name" => "Story 1", "id" => 1})
+        expect(subject).to receive(:get).with('projects/1/stories/1').and_return({"name" => "Story 1", "id" => 1})
         expect(story).to be_instance_of Story
 
         expect(story.id).to eq 1
         expect(story.name).to eq 'Story 1'
-      end
-
-      it 'raises an error when resource is unknown' do
-        expect{ subject.find(:tester, 1) }.to raise_error NoSuchResourceError, "Available resources are [:story, :stories]"
-      end
-    end
-
-    describe '#resource' do
-      it 'returns class name and endpoint as keys mapping to the class' do
-        expect(subject.known_resources).to eq({
-          stories: Clubhouse::Story,
-          story: Clubhouse::Story
-        })
       end
     end
   end
