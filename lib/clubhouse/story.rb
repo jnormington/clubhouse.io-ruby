f0c0e8a23de8eb9ae36bc4ff633c33c8cf4b48d4
Jon Normington <jnormington@users.noreply.github.com>
2016-09-13 22:48:19 +0100

    Add attributes for create/update to the story model

--- lib/clubhouse/story.rb
+++ lib/clubhouse/story.rb
@@ -7,5 +7,13 @@ module Clubhouse
       :position, :project_id, :requested_by_id, :story_links, :story_type, :tasks,
       :updated_at, :workflow_state_id, readonly: :id
 
+    attributes_for_create :comments, :created_at, :deadline, :description, :epic_id, :estimate,
+                          :external_id, :file_ids, :labels, :linked_file_ids, :name, :owner_ids,
+                          :project_id, :requested_by_id, :story_links, :story_type, :tasks,
+                          :updated_at, :workflow_state_id
+
+    attributes_for_update :after_id, :archived, :before_id, :deadline, :description, :epic_id,
+                          :estimate, :file_ids, :follower_ids, :labels, :linked_file_ids, :name,
+                          :owner_ids, :project_id, :requested_by_id, :story_type, :workflow_state_id
   end
 end
