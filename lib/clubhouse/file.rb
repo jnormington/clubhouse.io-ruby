a9c1307a1449c4f2983477807468c8f8f010de05
Jon Normington <jnormington@users.noreply.github.com>
2016-09-24 17:40:28 +0100

    Add the file resource

---
+++ lib/clubhouse/file.rb
@@ -0,0 +1,19 @@
+module Clubhouse
+  class File < BaseResource
+    resource :files
+
+    attributes :created_at, :description, :external_id, :name, :updated_at, :uploaded_id,
+      readonly: [:content_type, :filename, :id, :mention_ids, :size, :story_ids, :thumbnail_url, :url]
+
+    attributes_for_update :description, :external_id, :name, :updated_at, :uploaded_id
+
+    def save
+      if id.nil?
+        raise NotSupportedByAPIError,
+          "You can't create a direct file upload, but can create linked files over the API"
+      end
+
+      super
+    end
+  end
+end
