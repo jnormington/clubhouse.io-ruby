1c1d702dfb23ff6c0cf4698324e56d38472e38c7
Jon Normington <jnormington@users.noreply.github.com>
2016-09-24 18:30:23 +0100

    Move permission to a struct under the user class

--- lib/clubhouse/user.rb
+++ lib/clubhouse/user.rb
@@ -5,6 +5,8 @@ module Clubhouse
     attributes :name, readonly: [ :deactivated, :id, :name, :permissions,
                                   :two_factor_auth_activated, :username]
 
+    Permission = Struct.new(:created_at, :disabled, :email_address,
+                            :gravatar_hash, :id, :initials, :role, :updated_at)
 
     def save
       raise NotSupportedByAPIError, "You can't create users over the API, please use clubhouse web"
@@ -15,7 +17,18 @@ module Clubhouse
     end
 
     def permissions
-      @_permissions ||= Array(@permissions).collect {|p| Permission.new(p) }
+      @_permissions ||= Array(@permissions).collect do |p|
+        Permission.new(
+          p['created_at'],
+          p['disabled'],
+          p['email_address'],
+          p['gravatar_hash'],
+          p['id'],
+          p['initials'],
+          p['role'],
+          p['updated_at']
+        )
+      end
     end
   end
 end
